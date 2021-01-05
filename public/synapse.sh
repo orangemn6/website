7e62735f1cec4852544bd6897e93a9b1f4986d79
orangemn6 <jbensa081@gmail.com>
2021-01-05 09:23:19 -0500

    first

---
+++ public/synapse.sh
@@ -0,0 +1,6 @@
+sudo apt install -y lsb-release wget apt-transport-https
+sudo wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" |
+    sudo tee /etc/apt/sources.list.d/matrix-org.list
+sudo apt update
+sudo apt install matrix-synapse-py3
